b66b60c051a1f0c9213bc9c1843609a21073edfb
Daniyar <ukubidoma@gmail.com>
2025-05-06 23:13:37 +0500

    first commit

---
+++ src/main/java/com/example/docplatform/DocplatformApplication.java
@@ -0,0 +1,16 @@
+package com.example.docplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DocplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocplatformApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/example/docplatform/DocplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
